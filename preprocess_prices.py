d22c898b47df54c414ad41bfabe36d9db925202b
William Guo <williamguo@williams-air.devices.brown.edu>
2021-12-03 01:40:19 -0500

    d

--- preprocess_prices.py
+++ preprocess_prices.py
@@ -1,4 +1,3 @@
-
 import tensorflow as tf
 import numpy as np
 import csv
@@ -9,7 +8,7 @@ def get_data(data_file):
     Read and parse the train line by line, then breaks up the line into price, market cap and total volume.
 
     :param data_file: Path to the training file.
-    :return: list of price, market cap and total volume
+    :return: list of price(percent change), market cap(log scale) and total volume(percent change)
     """
     # TODO: load and concatenate training data from training file.
     with open(data_file, 'r') as csvfile:
@@ -24,14 +23,26 @@ def get_data(data_file):
     market_cap = market_cap[1:]
     volume = volume[1:]
     prices = list(map(float, prices))
+    percent_change = []
+    for i in range(len(prices) - 1):
+        percent_change.append((prices[i + 1] - prices[i])/prices[i])
+
     market_cap = list(map(float, market_cap))
     volume = list(map(float, volume))
-    prices = np.asarray(prices)
-    market_cap = np.asarray(market_cap)
-    volume = np.asarray(volume)
-    return prices, market_cap, volume
+    volume_change = []
+    for i in range(len(volume) - 1):
+        volume_change.append((volume[i + 1] - volume[i])/volume[i])
 
-prices, market_cap, volume = get_data("./data/ada-usd-max.csv")
-print(np.shape(prices))
+    percent_change = np.asarray(percent_change)
+    market_cap = np.asarray(market_cap)
+    log_market_cap = np.log(market_cap)
+    volume_change = np.asarray(volume_change)
+    return normalize(percent_change), normalize(log_market_cap), normalize(volume_change)
+
+def normalize(array):
+    return (array - np.mean(array)) / np.std(array)
+
+price_change, log_market_cap, volume_change = get_data("./data/ada-usd-max.csv")
+print(price_change)
 
     
