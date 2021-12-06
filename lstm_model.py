bdd57ae94719c3c3e33418ffcb0b1893d6c9ef37
dg314 <david_grossman@brown.edu>
2021-12-05 21:41:49 -0500

    update

--- lstm_model.py
+++ lstm_model.py
@@ -3,12 +3,15 @@ import numpy as np
 from preprocess_prices import get_data
 import os
 
+accs = []
+
 class Model(tf.keras.Model):
     def __init__(self):
         super(Model, self).__init__()
 
         self.window_size = 15
         self.window_shift = 1
+        self.batch_size = 30
 
         # Initialize all hyperparameters
         self.learning_rate = 0.005
@@ -18,7 +21,9 @@ class Model(tf.keras.Model):
         # Initialize all trainable parameters
         self.dense1 = tf.keras.layers.Dense(16)
         self.dense2 = tf.keras.layers.Dense(1)
-        self.gru = tf.keras.layers.GRU(64, return_sequences=True, return_state=False)
+        self.gru = tf.keras.layers.LSTM(64, return_sequences=True, return_state=False)
+        self.dense3 = tf.keras.layers.Dense(64)
+        self.dense4 = tf.keras.layers.Dense(1)
     
 
     def call(self, inputs, initial_state):
@@ -30,8 +35,9 @@ class Model(tf.keras.Model):
         :return: 
         """
         
-        fully_connected_output = tf.squeeze(self.dense2(self.dense1(inputs)))
-        return self.gru(fully_connected_output, initial_state=initial_state)
+        fully_connected_output = tf.squeeze(self.dense2(self.dense1(tf.convert_to_tensor(inputs))))
+        gru_output = self.gru(tf.reshape(fully_connected_output, [self.batch_size, self.window_size, 1]), initial_state=initial_state)
+        return self.dense4(self.dense3(gru_output))
 
 
     def loss(self, predictions, labels):
@@ -45,7 +51,7 @@ class Model(tf.keras.Model):
         return tf.reduce_mean(tf.keras.losses.mse(labels, predictions))
 
     def accuracy(self, predictions, labels):
-        return tf.math.mean((predictions >= 0) == (labels >= 0))
+        return tf.math.reduce_mean(tf.cast((predictions >= 0) == (labels >= 0), tf.float32))
 
 
 def train(model, inputs, initial_state):
@@ -58,23 +64,28 @@ def train(model, inputs, initial_state):
     :return: List of losses for every 50th window
     '''
 
-    losses = []
+    window_inputs = []
 
     for idx in range(0, tf.shape(inputs)[0] - model.window_size - 1, model.window_shift):
-        window_inputs = inputs[idx:idx+model.window_size]
-        window_labels = inputs[idx+1:idx+model.window_size+1]
+        window_inputs.append(inputs[idx:idx+model.window_size])
+
+    losses = []
+
+    for batch_idx in range(0, len(window_inputs) - model.batch_size - 1, model.batch_size):
+        batch_inputs = window_inputs[batch_idx:batch_idx+model.batch_size]
+        batch_labels = np.asarray(window_inputs[batch_idx+1:batch_idx+model.batch_size+1])[:, :, 0:1]
 
         with tf.GradientTape() as tape:
-            predictions = model(window_inputs, initial_state)
-            loss = model.loss(predictions, window_labels)
-        
+            predictions = model(batch_inputs, initial_state)
+            loss = model.loss(predictions, batch_labels)
+    
         gradients = tape.gradient(loss, model.trainable_variables)
         model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+        losses.append(loss)
+        #print(loss)
+        accs.append(model.accuracy(predictions, batch_labels))
 
-        if idx % 50 == 0:
-            losses.append(loss)
-
-    return losses
+    return tf.convert_to_tensor(losses)
 
 
 def test(model, test_inputs, initial_state):
@@ -94,7 +105,7 @@ def test(model, test_inputs, initial_state):
 
     for idx in range(0, tf.shape(test_inputs)[0] - model.window_size - 1, model.window_shift):
         window_inputs = test_inputs[idx:idx+model.window_size]
-        window_labels = test_inputs[idx+1:idx+model.window_size+1]
+        window_labels = np.asarray(test_inputs[idx+1:idx+model.window_size+1])[:, :, 0:1]
         predictions = model(window_inputs, initial_state)
         accuracies.append(model.accuracy(predictions, window_labels))
         
@@ -128,7 +139,9 @@ def main():
     for crypto, data in crypto_data.items():
         print("Training " + crypto)
         p = train(model, data, None)
-        print(data)
+        print(p)
+
+    print(tf.math.reduce_mean(accs))
     
 
 
